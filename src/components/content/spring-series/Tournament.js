1185aed3e36a4577dfcf602814733da59b83dcce
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-04-23 17:50:15 +0400

    cashGamesPage

---
+++ src/components/content/spring-series/Tournament.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Tournament =styled.div`
+     background-color:#171718;
+     border-radius:16px;
+     border:solid 4px #25292B;
+     width:100%;
+     max-width:846px;
+    
+
+`
